c2d2361577dcecdec35401d7e2711727db5e9843
David Griffiths <davidgriffiths@hotmail.com>
2023-01-30 21:21:42 +0000

    hurry up with the quote screen

--- src/views/quotes/ViewEditQuote.vue
+++ src/views/quotes/ViewEditQuote.vue
@@ -20,6 +20,29 @@
             </button>
         </template>
     </AddEditQuote>
+
+    <div class="card xhas-background-link-dark p-4 mb-5">
+        <label style="display: block" for="merchandising">        {{ storeCatalogue.getCatalogueOptions('cardMachine') }}</label>
+
+        <select
+            name="merchandising"
+            v-model="test"
+            @change="selectChanged(test)"
+
+        >
+        <option
+            v-for="catalogueItem in storeCatalogue.items"
+            :value= catalogueItem.price
+
+        >
+        {{ catalogueItem.name }}
+        </option>
+        </select>
+
+
+
+
+    </div>
 </template>
 
 <script setup>
@@ -27,17 +50,26 @@
     import { useRoute, useRouter } from 'vue-router'
     import AddEditQuote from '@/components/Quotes/AddEditQuote.vue'
     import { useStoreQuotes } from '@/stores/StoreQuotes'
+    import { useStoreCatalogue } from '@/stores/StoreCatalogue'
 
     let quote = ref({})
     const storeQuotes = useStoreQuotes()
+    const storeCatalogue = useStoreCatalogue()
     const route = useRoute()
     const router = useRouter()
+
+    let test = ref()
+    let options = storeCatalogue.getCatalogue('merchandising')
+    console.log('options', options)
 
     quote = storeQuotes.getQuote(route.params.id)
 
     const handleSavedClicked = () => {
         storeQuotes.updateQuote(route.params.id, quote)
         router.back()
+    }
+    const selectChanged = (event) => {
+console.log(event)
     }
 
 </script>
