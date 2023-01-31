de3ca0ff7a94fee42ef5a1ae088fdd9d780b98d1
David Griffiths <davidgriffiths@hotmail.com>
2023-01-31 07:45:43 +0000

    wip quote page

--- src/views/quotes/PreviewContract.vue
+++ src/views/quotes/PreviewContract.vue
@@ -39,13 +39,18 @@
 </template>
 
 <script setup>
-    import { normalizeStyle, ref, reactive } from 'vue'
+    import { normalizeStyle, ref, reactive, computed, watch, onMounted } from 'vue'
     import { useRoute, useRouter } from 'vue-router'
     import Quote from '@/components/Quotes/Quote.vue'
     import AddEditQuote from '@/components/Quotes/AddEditQuote.vue'
     import { useStoreQuotes } from '@/stores/StoreQuotes'
     import { useStoreLeads } from '@/stores/StoreLeads'
     import { useStoreSysVars } from '@/stores/StoreSysVars'
+    import { useStoreCatalogue } from '@/stores/StoreCatalogue'
+
+
+
+import { defineComponent } from 'vue';
 
     const route = useRoute()
     const router = useRouter()
@@ -55,7 +60,7 @@
     const storeLeads = useStoreLeads()
     const storeSysVars = useStoreSysVars()
 
-    const quote = storeQuotes.getQuote(route.params.id)
+    const oldquote = storeQuotes.getQuote(route.params.id)
     const lead = storeLeads.getLead(quote.leadId)
 
     let varVal = ref(null)
@@ -73,6 +78,368 @@
         console.log(varVal.value)
 
     }
+
+
+
+const catalogueStore = useStore(Catalogue)
+
+
+
+
+
+
+
+
+const catalogue = reactive(catalogueStore.catalogue)
+const options = ['a','b']
+const value = ref()
+const brand = ref()
+const model = ref()
+const plumbing = ref()
+
+let quote = reactive (
+    {
+    "machine": {
+      "model": "xx",
+      "variant": "",
+      "rrPrice": 0,
+      "wsPrice": 0,
+      "profit": 0
+    },
+    "waterOption": {
+      "description": "",
+      "variant": "",
+      "rrPrice": 0,
+      "wsPrice": 0,
+      "profit": 0
+    },
+    "warranty": {
+      "term": "",
+      "cover": "",
+      "rrPrice": 0,
+      "wsPrice": 0,
+      "profit": 0
+    },
+    "cardMachine": {
+      "package": "0",
+      "variant": "",
+      "rrPrice": 0,
+      "wsPrice": 0,
+      "profit": 0
+    },
+    "marketing": {
+      "package": "0",
+      "variant": "",
+      "rrPrice": 0,
+      "wsPrice": 0,
+      "profit": 0
+    },
+    "merchandising": {
+      "package": "0",
+      "variant": "",
+      "rrPrice": 0,
+      "wsPrice": 0,
+      "profit": 0
+    },
+    "drinkPack": {
+      "package": "0",
+      "variant": "",
+      "rrPrice": 0,
+      "wsPrice": 0,
+      "profit": 0
+    },
+    "subTotal": {
+      "model": "",
+      "variant": "",
+      "rrPrice": 0,
+      "wsPrice": 0,
+      "profit": 0
+    },
+    "discount": {
+      "percentage": 0,
+      "variant": "",
+      "rrPrice": 0,
+      "wsPrice": 0,
+      "profit": 0
+    },
+    "additions": {
+      "model": "",
+      "variant": "",
+      "rrPrice": 0,
+      "wsPrice": 0,
+      "profit": 0
+    },
+    "total": {
+      "model": "",
+      "variant": "",
+      "rrPrice": 0,
+      "wsPrice": 0,
+      "profit": 0
+    },
+    "finance": {
+      "model": "",
+      "variant": "",
+      "rrPrice": 0,
+      "wsPrice": 0,
+      "profit": 0
+    },
+    "financeFee": {
+      "fee": "",
+      "variant": "",
+      "rrPrice": 0,
+      "wsPrice": 0,
+      "profit": 0
+    },
+    "finalPrice": {
+      "model": "",
+      "variant": "",
+      "rrPrice": 0,
+      "wsPrice": 0,
+      "profit": 0
+    },
+    "financeBreakdown": {
+      "model": "",
+      "monthly": "",
+      "weekly": 0,
+      "daily7": 0,
+      "daily5": 0
+    },
+    "commission": {
+      "leadSource": "",
+      "providedLead": 0,
+      "selfGenerated": 0,
+      "percentageOfDeal": 0,
+      "status": "closed",
+      "success": "won"
+    }
+  }
+)
+
+quote.cardMachine.rrPrice = computed(() => {
+    for (let option in catalogue) {
+        if (quote.cardMachine.package == catalogue[option].id) {
+            return catalogue[option].field.rrPrice
+        }
+    }
+})
+
+quote.cardMachine.wsPrice = computed(() => {
+    for (let option in catalogue) {
+        if (quote.cardMachine.package == catalogue[option].id) {
+            return catalogue[option].field.wsPrice
+        }
+    }
+})
+
+quote.marketing.rrPrice = computed(() => {
+    for (let option in catalogue) {
+        if (quote.marketing.package == catalogue[option].id) {
+            return catalogue[option].field.rrPrice
+        }
+    }
+})
+
+quote.marketing.wsPrice = computed(() => {
+    for (let option in catalogue) {
+        if (quote.marketing.package == catalogue[option].id) {
+            return catalogue[option].field.wsPrice
+        }
+    }
+})
+
+quote.merchandising.rrPrice = computed(() => {
+    for (let option in catalogue) {
+        if (quote.merchandising.package == catalogue[option].id) {
+            return catalogue[option].field.rrPrice
+        }
+    }
+})
+
+quote.merchandising.wsPrice = computed(() => {
+    for (let option in catalogue) {
+        if (quote.merchandising.package == catalogue[option].id) {
+            return catalogue[option].field.wsPrice
+        }
+    }
+})
+
+quote.drinkPack.rrPrice = computed(() => {
+    for (let option in catalogue) {
+        if (quote.drinkPack.package == catalogue[option].id) {
+            return catalogue[option].field.rrPrice
+        }
+    }
+})
+
+quote.drinkPack.wsPrice = computed(() => {
+    for (let option in catalogue) {
+        if (quote.drinkPack.package == catalogue[option].id) {
+            return catalogue[option].field.wsPrice
+        }
+    }
+})
+
+quote.machine.profit = computed(() => {
+    return parseInt(quote.machine.rrPrice) - parseInt(quote.machine.wsPrice)
+})
+quote.waterOption.profit = computed(() => {
+    return parseInt(quote.waterOption.rrPrice) - parseInt(quote.waterOption.wsPrice)
+})
+quote.warranty.profit = computed(() => {
+    return parseInt(quote.warranty.rrPrice) - parseInt(quote.warranty.wsPrice)
+})
+quote.cardMachine.profit = computed(() => {
+    return parseInt(quote.cardMachine.rrPrice) - parseInt(quote.cardMachine.wsPrice)
+})
+quote.marketing.profit = computed(() => {
+    return parseInt(quote.marketing.rrPrice) - parseInt(quote.marketing.wsPrice)
+})
+quote.merchandising.profit = computed(() => {
+    return parseInt(quote.merchandising.rrPrice) - parseInt(quote.merchandising.wsPrice)
+})
+quote.drinkPack.profit = computed(() => {
+    return parseInt(quote.drinkPack.rrPrice) - parseInt(quote.drinkPack.wsPrice)
+})
+quote.subTotal.profit = computed(() => {
+    return parseInt(quote.subTotal.rrPrice) - parseInt(quote.subTotal.wsPrice)
+})
+quote.discount.rrPrice = computed(() => {
+    return parseInt( - quote.subTotal.rrPrice) * parseInt(quote.discount.percentage) / 100
+})
+quote.discount.profit = computed(() => {
+    return parseInt(quote.discount.rrPrice)
+})
+quote.additions.profit = computed(() => {
+    return parseInt(quote.additions.rrPrice) - parseInt(quote.additions.wsPrice)
+})
+quote.total.profit = computed(() => {
+    return parseInt(quote.total.rrPrice) - parseInt(quote.total.wsPrice)
+})
+quote.finance.profit = computed(() => {
+    return parseInt(quote.finance.rrPrice) - parseInt(quote.finance.wsPrice)
+})
+quote.financeFee.profit = computed(() => {
+    return parseInt(quote.financeFee.rrPrice) - parseInt(quote.financeFee.wsPrice)
+})
+quote.finalPrice.profit = computed(() => {
+    return parseInt(quote.finalPrice.rrPrice) - parseInt(quote.finalPrice.wsPrice)
+})
+quote.financeBreakdown.monthly = computed(() => {
+    return parseInt(quote.finalPrice.rrPrice) / 12 / parseInt(quote.finance.term)
+})
+quote.financeBreakdown.weekly = computed(() => {
+    return parseInt(quote.finalPrice.rrPrice) / 52 / parseInt(quote.finance.term)
+})
+quote.financeBreakdown.daily7 = computed(() => {
+    return parseInt(quote.finalPrice.rrPrice) / 52 / 7 / parseInt(quote.finance.term)
+})
+quote.financeBreakdown.daily5 = computed(() => {
+    return parseInt(quote.finalPrice.rrPrice) / 52 / 5 / parseInt(quote.finance.term)
+})
+quote.commission.providedLead = computed(() => {
+    return parseInt(quote.finalPrice.rrPrice) / 100 * 50
+})
+quote.commission.selfGenerated = computed(() => {
+    return parseInt(quote.finalPrice.rrPrice) / 100 * 75
+})
+// TODO: Put actual lead source in here
+quote.commission.percentageOfDeal = computed(() => {
+    return (
+        parseInt(quote.finalPrice.rrPrice) ?
+        parseInt(quote.commission.selfGenerated) / parseInt(quote.finalPrice.rrPrice) * 100 :
+        0
+    )
+})
+
+quote.subTotal.rrPrice = computed(() => {
+    const t =
+        parseInt(quote.machine.rrPrice) +
+        parseInt(quote.waterOption.rrPrice) +
+        parseInt(quote.warranty.rrPrice) +
+        parseInt(quote.cardMachine.rrPrice) +
+        parseInt(quote.marketing.rrPrice) +
+        parseInt(quote.merchandising.rrPrice) +
+        parseInt(quote.drinkPack.rrPrice)
+    return t
+})
+quote.subTotal.wsPrice = computed(() => {
+    const t =
+        parseInt(quote.machine.wsPrice) +
+        parseInt(quote.waterOption.wsPrice) +
+        parseInt(quote.warranty.wsPrice) +
+        parseInt(quote.cardMachine.wsPrice) +
+        parseInt(quote.marketing.wsPrice) +
+        parseInt(quote.merchandising.wsPrice) +
+        parseInt(quote.drinkPack.wsPrice)
+    return t
+})
+
+quote.total.rrPrice = computed(() => {
+    const t =
+        parseInt(quote.machine.rrPrice) +
+        parseInt(quote.waterOption.rrPrice) +
+        parseInt(quote.warranty.rrPrice) +
+        parseInt(quote.cardMachine.rrPrice) +
+        parseInt(quote.marketing.rrPrice) +
+        parseInt(quote.merchandising.rrPrice) +
+        parseInt(quote.drinkPack.rrPrice) +
+        parseInt(quote.discount.rrPrice) +
+        parseInt(quote.additions.rrPrice)
+
+    return t
+})
+quote.total.wsPrice = computed(() => {
+    const t =
+    parseInt(quote.machine.wsPrice) +
+        parseInt(quote.waterOption.wsPrice) +
+        parseInt(quote.warranty.wsPrice) +
+        parseInt(quote.cardMachine.wsPrice) +
+        parseInt(quote.marketing.wsPrice) +
+        parseInt(quote.merchandising.wsPrice) +
+        parseInt(quote.drinkPack.wsPrice) +
+        parseInt(quote.discount.wsPrice) +
+        parseInt(quote.additions.wsPrice)
+    return t
+})
+
+quote.finalPrice.rrPrice = computed(() => {
+    const t =
+        parseInt(quote.machine.rrPrice) +
+        parseInt(quote.waterOption.rrPrice) +
+        parseInt(quote.warranty.rrPrice) +
+        parseInt(quote.cardMachine.rrPrice) +
+        parseInt(quote.marketing.rrPrice) +
+        parseInt(quote.merchandising.rrPrice) +
+        parseInt(quote.drinkPack.rrPrice) +
+        parseInt(quote.discount.rrPrice) +
+        parseInt(quote.additions.rrPrice) +
+        parseInt(quote.financeFee.rrPrice) +
+        parseInt(quote.finance.rrPrice)
+
+
+    return t
+})
+quote.finalPrice.wsPrice = computed(() => {
+    const t =
+    parseInt(quote.machine.wsPrice) +
+        parseInt(quote.waterOption.wsPrice) +
+        parseInt(quote.warranty.wsPrice) +
+        parseInt(quote.cardMachine.wsPrice) +
+        parseInt(quote.marketing.wsPrice) +
+        parseInt(quote.merchandising.wsPrice) +
+        parseInt(quote.drinkPack.wsPrice) +
+        parseInt(quote.discount.wsPrice) +
+        parseInt(quote.additions.wsPrice) +
+        parseInt(quote.financeFee.wsPrice) +
+        parseInt(quote.finance.wsPrice)
+    return t
+})
+
+// TODO: delete this
+quote.finance.term = 3
+
+const actual = ref(null)
+
 
 
 
--- src/views/quotes/ViewEditQuote.vue
+++ src/views/quotes/ViewEditQuote.vue
@@ -32,7 +32,7 @@
         >
         <option
             v-for="catalogueItem in storeCatalogue.items"
-            :value= catalogueItem.price
+            :value= catalogueItem.description
 
         >
         {{ catalogueItem.name }}
@@ -43,6 +43,636 @@
 
 
     </div>
+    <br><br>
+
+    <div v-if="quote" class="quote-container ">
+
+<!-- Start Row -->
+<div class="font-bold">
+    Machine
+</div>
+
+<div>
+
+</div>
+
+<div class="font-bold">
+    RRP
+</div>
+
+<div class="font-bold">
+    WSP
+</div>
+
+<div class="font-bold">
+    NET
+</div>
+<!-- End Row -->
+
+
+<!-- Start Row -->
+<div>
+    <select v-model="quote" label="Model" :options="options" />
+</div>
+
+<div>
+    <select v-model="quote.machine.model.variant" label="Variant" :options="options" />
+</div>
+
+<div class="rrp">
+    <input readonly v-model="quote.machine.rrp" />
+</div>
+
+<div class="wsp">
+    <input readonly v-model="quote.machine.wsp" />
+</div>
+
+<div class="net">
+    <input readonly v-model="quote.machine.net" />
+</div>
+<!-- End Row -->
+
+<!-- Start Row -->
+<div>
+    <select v-model="quote.waterOption.description" label="Water Option" :options="options" />
+</div>
+
+<div>
+
+</div>
+
+<div>
+    <input readonly v-model="quote.waterOption.rrp" />
+</div>
+
+<div class="wsp">
+    <input readonly v-model="quote.waterOption.wsp" />
+</div>
+
+<div class="net">
+    <input readonly v-model="quote.waterOption.net" />
+</div>
+<!-- End Row -->
+
+<!-- Start Row -->
+<div class="font-bold">
+    Warranty
+</div>
+
+<div>
+
+</div>
+
+<div>
+
+</div>
+
+<div class="wsp">
+
+</div>
+
+<div class="net">
+
+</div>
+<!-- End Row -->
+
+<!-- Start Row -->
+<div class="text-green-800 font-semibold">
+    <select v-model="quote.warranty.term" label="Term" :options="options" />
+</div>
+
+<div class="text-green-800 font-semibold">
+    <select v-model="quote.warranty.term.cover" label="Cover" :options="options" />
+</div>
+
+<div>
+    <input readonly v-model="quote.warranty.rrp" />
+</div>
+
+<div class="wsp">
+    <input readonly v-model="quote.warranty.wsp" />
+</div>
+
+<div class="net">
+    <input readonly v-model="quote.warranty.net" />
+</div>
+<!-- End Row -->
+
+<!-- Start Row -->
+<div class="font-bold">
+    Options
+</div>
+
+<div>
+
+</div>
+
+<div>
+
+</div>
+
+<div class="wsp">
+
+</div>
+
+<div class="net">
+
+</div>
+<!-- End Row -->
+
+<!-- Start Row -->
+<div class="text-green-800 font-semibold">
+    <select
+        v-model="quote.cardMachine.package"
+        label="Card Machine"
+        :options="catalogue"
+        :text-by="(catalogue)=>catalogue.field.description"
+        :value-by="(catalogue)=>catalogue.id"
+    />
+</div>
+
+<div class="text-green-800 font-semibold">
+
+</div>
+
+<div>
+    <input readonly v-model="quote.cardMachine.rrp" />
+</div>
+
+<div class="wsp">
+    <input readonly v-model="quote.cardMachine.wsp" />
+</div>
+
+<div class="net">
+    <input readonly v-model="quote.cardMachine.net" />
+</div>
+<!-- End Row -->
+
+
+<!-- Start Row -->
+<div class="text-green-800 font-semibold">
+    <select
+        v-model="quote.marketing.package"
+        label="Marketing Pack"
+        :options="options"
+        :text-by="(catalogue)=>catalogue.field.description"
+        :value-by="(catalogue)=>catalogue.id"
+    />
+</div>
+
+<div class="text-green-800 font-semibold">
+
+</div>
+
+<div>
+    <input readonly v-model="quote.marketing.rrp" />
+</div>
+
+<div class="wsp">
+    <input readonly v-model="quote.marketing.wsp" />
+</div>
+
+<div class="net">
+    <input readonly v-model="quote.marketing.net" />
+</div>
+<!-- End Row -->
+
+<!-- Start Row -->
+<div class="text-green-800 font-semibold">
+
+    <!-- TODO: jiojodd -->
+    <select
+        v-model="quote.merchandising.package"
+        label="Merchandising"
+        :options="catalogue"
+        :text-by="(catalogue)=>catalogue.field.description"
+        :value-by="(catalogue)=>catalogue.id"
+    />
+  </div>
+
+<div class="text-green-800 font-semibold">
+
+</div>
+
+<div class="rrp">
+    <input readonly v-model="quote.merchandising.rrp" />
+</div>
+
+<div class="wsp">
+    <input readonly initial="0" v-model="quote.merchandising.wsp" />
+</div>
+
+<div class="net">
+    <input readonly v-model="quote.merchandising.net" />
+</div>
+<!-- End Row -->
+
+<!-- Start Row -->
+<div class="text-green-800 font-semibold">
+    <select
+        v-model="quote.drinkPack.package"
+        label="Drink Pack"
+        :options="options"
+        :text-by="(catalogue)=>catalogue.field.description"
+        :value-by="(catalogue)=>catalogue.id"
+    />
+</div>
+
+<div class="text-green-800 font-semibold">
+
+</div>
+
+<div>
+    <input readonly v-model="quote.drinkPack.rrp" />
+</div>
+
+<div class="wsp">
+    <input readonly v-model="quote.drinkPack.wsp" />
+</div>
+
+<div class="net">
+    <input readonly v-model="quote.drinkPack.net" />
+</div>
+<!-- End Row -->
+
+<!-- Start Row -->
+<div class="font-bold">
+    Subtotal
+</div>
+
+<div>
+
+</div>
+
+<div class="font-bold">
+    RRP
+</div>
+
+<div class="font-bold">
+    WSP
+</div>
+
+<div class="font-bold">
+    net
+</div>
+<!-- End Row -->
+
+<!-- Start Row -->
+<div class="font-bold">
+
+</div>
+
+<div>
+
+</div>
+
+<div class="font-bold">
+    <input readonly v-model="quote.subTotal.rrp" />
+</div>
+
+<div class="font-bold">
+    <input readonly v-model="quote.subTotal.wsp" />
+</div>
+
+<div class="font-bold">
+    <input readonly v-model="quote.subTotal.net" />
+</div>
+<!-- End Row -->
+
+<!-- Start Row -->
+<div class="font-bold">
+    Adjustments
+</div>
+
+<div class="text-green-800 font-semibold">
+
+</div>
+
+<div>
+
+</div>
+
+<div class="wsp">
+
+</div>
+
+<div class="net">
+
+</div>
+<!-- End Row -->
+
+
+
+
+<!-- Start Row -->
+<div class="text-green-800 font-semibold">
+    <input v-model="quote.additions.rrp" label="Additions" />
+</div>
+
+<div class="text-green-800 font-semibold">
+
+</div>
+
+<div>
+    <input readonly v-model="quote.additions.rrp" />
+</div>
+
+<div class="wsp">
+    <input readonly v-model="quote.additions.wsp" />
+</div>
+
+<div class="net">
+    <input readonly v-model="quote.additions.net" />
+</div>
+<!-- End Row -->
+
+<!-- Start Row -->
+<div class="text-green-800 font-semibold">
+    <input v-model="quote.discount.percentage" label="Discount %" />
+</div>
+
+<div class="text-green-800 font-semibold">
+
+</div>
+
+<div>
+    <input readonly v-model="quote.discount.rrp" />
+</div>
+
+<div class="wsp">
+
+</div>
+
+<div class="net">
+    <input readonly v-model="quote.discount.net" />
+</div>
+<!-- End Row -->
+
+<!-- Start Row -->
+<div class="font-bold">
+    Total
+</div>
+
+<div>
+
+</div>
+
+<div class="font-bold">
+    RRP
+</div>
+
+<div class="font-bold">
+    WSP
+</div>
+
+<div class="font-bold">
+    net
+</div>
+<!-- End Row -->
+
+
+<!-- Start Row -->
+<div class="font-bold">
+
+</div>
+
+<div>
+
+</div>
+
+<div class="font-bold">
+    <input readonly v-model="quote.total.rrp" />
+</div>
+
+<div class="font-bold">
+    <input readonly v-model="quote.total.wsp" />
+</div>
+
+<div class="font-bold">
+    <input readonly v-model="quote.total.net" />
+</div>
+<!-- End Row -->
+
+
+<!-- Start Row -->
+<div class="font-bold">
+    Finance (est.)
+</div>
+
+<div>
+
+</div>
+
+<div>
+
+</div>
+
+<div class="wsp">
+
+</div>
+
+<div class="net">
+
+</div>
+<!-- End Row -->
+
+<!-- Start Row -->
+<div class="text-green-800 font-semibold">
+    <select v-model="quote.finance.term" label="Term" :options="options" />
+</div>
+
+<div class="text-green-800 font-semibold">
+
+</div>
+
+<div>
+    <input readonly v-model="quote.finance.rrp" />
+</div>
+
+<div class="wsp">
+    <input readonly v-model="quote.finance.wsp" />
+</div>
+
+<div class="net">
+    <input readonly v-model="quote.finance.net" />
+</div>
+<!-- End Row -->
+
+<!-- Start Row -->
+<div class="text-green-800 font-semibold">
+    <input readonly v-model="quote.financeFee.fee" label="Acceptance Fee???" />
+</div>
+
+<div class="text-green-800 font-semibold">
+
+</div>
+
+<div>
+    <input readonly v-model="quote.financeFee.fee" />
+</div>
+
+<div class="wsp">
+    <input readonly v-model="quote.financeFee.wsp" />
+</div>
+
+<div class="net">
+    <input readonly v-model="quote.financeFee.net" />
+</div>
+<!-- End Row -->
+
+
+
+<!-- Start Row -->
+<div class="font-bold">
+    Final Price
+</div>
+
+<div>
+
+</div>
+
+<div class="font-bold">
+    RRP
+</div>
+
+<div class="font-bold">
+    WSP
+</div>
+
+<div class="font-bold">
+    net
+</div>
+<!-- End Row -->
+
+
+<!-- Start Row -->
+<div class="font-bold">
+
+</div>
+
+<div>
+
+</div>
+
+<div class="font-bold">
+    <input readonly v-model="quote.finalPrice.rrp" />
+</div>
+
+<div class="font-bold">
+    <input readonly v-model="quote.finalPrice.wsp" />
+</div>
+
+<div class="font-bold">
+    <input readonly v-model="quote.finalPrice.net" />
+</div>
+<!-- End Row -->
+
+
+<!-- Put this in footer -->
+<!-- Start Row -->
+<div class="font-bold">
+    Finance Breakdown
+</div>
+
+<div class="font-bold">
+    Monthly
+</div>
+
+<div class="font-bold two-col-col">
+    <div>Weekly</div>
+    <div>Cups</div>
+</div>
+<div class="font-bold">
+    Daily 7
+</div>
+<div class="font-bold">
+    Daily 5
+</div>
+<!-- End Row -->
+
+
+<!-- Start Row -->
+<div class="font-bold">
+    Tier 1
+</div>
+
+<div class="font-bold">
+    <input readonly v-model="quote.financeBreakdown.monthly" />
+</div>
+<div class="font-bold two-col-col">
+    <div>
+    <input class="border border-solid border-gray-300 block mb-2 text-sm font-medium text-gray-900" readonly v-model="quote.financeBreakdown.weekly" />
+    </div>
+    <div>
+    <input readonly v-model="quote.financeBreakdown.weekly" />
+    </div>
+</div>
+
+<div class="font-bold">
+    <input readonly v-model="quote.financeBreakdown.daily7" />
+</div>
+
+<div class="font-bold">
+    <input readonly v-model="quote.financeBreakdown.daily5" />
+</div>
+<!-- End Row -->
+
+<!-- Put this in footer -->
+<!-- Start Row -->
+<div class="font-bold">
+    Commission
+</div>
+
+<div>
+
+</div>
+
+<div class="font-bold">
+    Provided Lead
+</div>
+<div class="font-bold">
+    Self Generated
+</div>
+<div class="font-bold">
+    % of Deal
+</div>
+<!-- End Row -->
+
+
+
+<!-- Start Row -->
+<div class="font-bold">
+    <input readonly v-model="quote.commission.leadSource" />
+</div>
+
+<div class="font-bold">
+
+</div>
+<div class="font-bold">
+    <input readonly v-model="quote.commission.providedLead" />
+</div>
+
+<div class="font-bold">
+    <input readonly v-model="quote.commission.selfGenerated" />
+</div>
+
+<div class="font-bold">
+    <input readonly v-model="quote.commission.percentageOfDeal" />
+</div>
+<!-- End Row -->
+
+
+
+
+
+
+
+
+
+
+</div>
+
+
 </template>
 
 <script setup>
